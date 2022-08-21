913190317e17f7715a8e8bc7427db1f5f73b46c9
DongMinhQuoc <98574052+RootsLambda@users.noreply.github.com>
2022-08-21 15:30:11 +0700

    Add files via upload

---
+++ textmixin0.py
@@ -0,0 +1,16 @@
+# File textmixin0.py
+class Super:
+    def __init__(self): # Superclass __init__
+        self.data1 = 'spam' # Create instance attrs
+    def ham(self):
+        pass
+class Sub(Super, ListInstance): # Mix in ham and a __str__
+    def __init__(self): # Listers have access to self
+        Super.__init__(self)
+        self.data2 = 'eggs' # More instance attrs
+        self.data3 = 42
+    def spam(self): # Define another method here
+        pass
+if __name__ == '__main__':
+    X = Sub()
+    print(X) # __str__
